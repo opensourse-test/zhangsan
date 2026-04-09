767cbbda9524e441119ccc3a4d3f35758bca95fe
coder1992 <462326676@qq.com>
2026-04-09 14:27:42 +0800

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,9 @@
+num=int(input("输入一个10以内的整数:"))
+if num==int(input("请输入第一次猜的数字:")):
+    print("恭喜你猜对了")
+elif num==int(input("不对，再猜一次:")):
+    print("恭喜你猜对了")
+elif num==int(input("不对，再猜最后一次:")):
+    print("恭喜你猜对了")
+else :
+    print("很遗憾，三次都猜错了，我心里的数字是",num)
